e5a6210ff71bfcc2ca5e01d7eed3efc638606a1a
Paul Schweppe <ps@cct-marketing.com>
2023-01-17 15:56:49 +0000

    [ET-590] Add Analytics client

---
+++ src/Analytics/AnalyticsClient.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CCT\SDK\Analytics;
+
+use CCT\SDK\Analytics\Response\CampaignAnalytics;
+use CCT\SDK\Campaign\Data\CampaignId;
+use CCT\SDK\Client\AbstractServiceClient;
+use CCT\SDK\Customer\Data\CustomerId;
+use CCT\SDK\Infrastucture\ValueObject\AbstractUrlOption;
+use GuzzleHttp\Psr7\Request;
+
+final class AnalyticsClient extends AbstractServiceClient
+{
+    public function campaignAnalytics(CustomerId $customerId, CampaignId $campaignId): ?CampaignAnalytics
+    {
+        $request = new Request(
+            'GET',
+            sprintf('api-v2/customers/%s/campaigns/%s/analytics', $customerId->toString(), $campaignId->toString())
+        );
+
+        $data = $this->sendJsonRequest($request, 200);
+
+        return CampaignAnalytics::fromArray($data[0]);
+    }
+
+    public function host(): AbstractUrlOption
+    {
+        return $this->options->analyticsHost;
+    }
+}
