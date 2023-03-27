453bee0ca3c2ee809cce2d70736b38716bb70d3b
Paul Schweppe <ps@cct-marketing.com>
2023-03-27 15:17:04 +0100

    Update examples with pause/resume campaign and get campaign state
    To better understand how to use the API, we have added helpful new examples that demonstrate how to get the campaign state, pause and resume campaigns. These simple examples will provide a clearer picture of how the API works and how it can be utilized.

---
+++ examples/Campaign/GetCampaignState.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CCT\SDK\Examples\Campaign;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use CCT\SDK\Campaign\Data\CampaignId;
+use CCT\SDK\Client\ClientFactory;
+use CCT\SDK\Customer\Data\CustomerId;
+use CCT\SDK\Examples\OptionsForExamples;
+use CCT\SDK\Exception\ApiExceptionInterface;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+
+$option = OptionsForExamples::create();
+
+$cache = new ArrayAdapter();
+
+$client = ClientFactory::create($option, $cache);
+
+$customerId = CustomerId::fromString('{CUSTOMER_ID}');
+$campaignId = CampaignId::fromString('{CAMPAIGN_ID}');
+
+try {
+    $response = $client->campaignClient()->getCampaignState($customerId, $campaignId);
+    printf('Campaign "%s" has state "%s" %s', $response->campaignId->toString(), $response->state->value, PHP_EOL);
+} catch (IdentityProviderException $e) {
+    printf('Auth error: %s', $e->getMessage());
+    exit(1);
+} catch (ApiExceptionInterface $e) {
+    printf('Api Error: %s', $e->getMessage());
+    exit(1);
+}
